c393cc5eb201e7bceedb41c1b8103fa062b2c206
Thlimass <thlima.silva.souza@gmail.com>
2024-04-25 17:16:11 -0300

    add style to the viewStudentsScreen

--- screens/ViewStudentsScreen.js
+++ screens/ViewStudentsScreen.js
@@ -1,16 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Content } from 'react-native';
 
 const ViewStudentsScreen = ({ route }) => {
   const { matricula, nome, media } = route.params;
 
   return (
-    <View>
-      <Text>Matrícula: {matricula}</Text>
-      <Text>Nome: {nome}</Text>
-      <Text>Média: {media}</Text>
+  <View style={styles.container}>
+      <Text style={styles.titleView}>Informações do Aluno</Text>
+      <View style={styles.view}>
+        <Text style={styles.textLabel}>Matrícula:</Text>
+        <Text style={styles.textContent}>{matricula}</Text>
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.textLabel}>Nome:</Text>
+        <Text style={styles.textContent}>{nome}</Text>
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.textLabel}>Média:</Text>
+        <Text style={styles.textContent}>{media}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#27282D',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:20
+  },
+  titleView: {
+  paddingTop:15,
+  color:'#fff',
+  fontSize:20,
+  fontWeight:'bold',
+  marginBottom:50
+  },
+  view: {
+  width:'100%',
+  flexDirection:'row',
+  borderBottomColor:'#c0c0c0',
+  borderBottomWidth:1,
+  marginBottom: 10
+  },
+  textLabel: {
+  color:'white',
+  fontWeight:'bold',
+  paddingLeft:10,
+  marginBottom: 10
+  },
+  textContent: {
+    color:'white',
+    paddingLeft:10
+  }
+});
 
 export default ViewStudentsScreen;
