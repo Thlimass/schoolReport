4d5fd11d440c430db9f5180b89bbd306579c2f94
Thlimass <thlima.silva.souza@gmail.com>
2024-04-25 16:07:02 -0300

    configuring routes in the App.js file

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AddStudentScreen from './screens/AddStudentScreen';
+import ViewStudentsScreen from './screens/ViewStudentsScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+      <Stack.Navigator initialRouteName="AddStudent">
+        <Stack.Screen name="AddStudent" component={AddStudentScreen} options={{ title: 'Adicionar Aluno' }} />
+        <Stack.Screen name="ViewStudents" component={ViewStudentsScreen} options={{ title: 'Alunos Registrados' }} />
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
